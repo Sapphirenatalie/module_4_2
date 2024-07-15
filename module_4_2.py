aac4a8a1ba352cc3ae16aee1c89ad5e8b9234016
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-15 23:03:10 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,26 @@
+# Пространство имен
+
+# Ваша задача:
+# Создайте новую функцию def test_function
+# Создайте другую функцию внутри функции inner_function,
+# функция должна печатать значение "Я в области видимости функции test_function"
+
+# Вызовите функцию inner_function внутри функции test_function
+# Попробуйте вызывать inner_function вне функции test_function
+# и посмотрите на результат выполнения программы
+
+
+d = 1
+
+
+def test_function():
+    d = 10
+
+    def inner_function():
+        d = 'local'
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+test_function()
